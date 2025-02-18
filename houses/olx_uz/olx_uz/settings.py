b9b4210df7e679aafbb866af3364182203cb47f5
Gulimoh <gulimoh@MacBook-Pro.local>
2025-02-18 15:13:12 +0500

    add

--- houses/olx_uz/olx_uz/settings.py
+++ houses/olx_uz/olx_uz/settings.py
@@ -64,7 +64,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    "olx_uz.pipelines.ApartmentPipeline": 300,
+    "olx_uz.pipelines.HousesPipeline": 300,
     "olx_uz.pipelines.PostgresqlPipeline": 400,
 }
 
